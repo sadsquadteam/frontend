227b1ebddfd4b2c29453931b17d90998d154bbd3
melikaalizade <melikaalizadeh7@gmail.com>
2026-01-02 10:52:44 +0330

    add logo

--- frontend/lost-and-found/src/components/Sidebar/Sidebar.jsx
+++ frontend/lost-and-found/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import SidebarNav from './SidebarNav';
+import logo from '../../assets/images/Logo-icon.svg';  // Adjust the path to your logo.svg file
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <div className="sidebar__logo">logo</div> {/* Updated class name */}
+      <div className="sidebar__logo">
+        <img src={logo} alt="Logo" /> {/* Display the logo icon */}
+      </div>
       <SidebarNav />
       <div className="avatar" />
     </aside>
