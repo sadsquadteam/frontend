b9c4b9932b82138049b6fc2d76c03df42979e8f9
Elina <hozhabri.elen@gmail.com>
2026-01-05 02:34:31 +0330

    add logoText

--- frontend/lost-and-found/src/components/Sidebar/Sidebar.jsx
+++ frontend/lost-and-found/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SidebarNav from './SidebarNav';
 import logo from '../../assets/images/Logo-icon.svg'; 
+import logoText from '../../assets/images/logo-text.svg';
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
       <div className="sidebar__logo">
         <img src={logo} alt="Logo" />
+        <img src={logoText} alt="Logo Text" />
       </div>
       <SidebarNav />
       <div className="avatar" />
